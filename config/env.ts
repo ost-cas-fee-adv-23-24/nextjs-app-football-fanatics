0ebf6e7513608c4b1bba8ea78fe8a7729b01356b
Patrick Lehmann <patrick.lehmann@intergga.ch>
2024-01-16 13:38:55 +0100

    check env variables with zod

---
+++ config/env.ts
@@ -0,0 +1,16 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import z from 'zod';
+
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(['development', 'production', 'test'])
+    .default('development'),
+  PORT: z.string().default('3000'),
+  ZITADEL_CLIENT_ID: z.string(),
+});
+
+const envVariables = envSchema.parse(process.env);
+
+export default envVariables;
