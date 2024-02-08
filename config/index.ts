a7ae964fae2cf40d9b7e645c121165b742416193
Bladimir Ardiles <bladimir.ardilescruz@swisscom.com>
2024-02-08 21:46:48 +0100

    feat: starts adding configs

---
+++ config/index.ts
@@ -0,0 +1,7 @@
+const mainConfig = {
+  zitadel: {
+    clientId: process.env.ZITADEL_CLIENT_ID,
+    authority: 'https://cas-fee-adv-ed1ide.zitadel.cloud',
+    codeVerifier: 'this-is-very-secret',
+  },
+};
