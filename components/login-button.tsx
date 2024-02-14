d421b0e4b1a112965c501644bdfc830df7d31e95
Bladimir Ardiles <bladimir.ardilescruz@swisscom.com>
2024-02-14 21:33:47 +0100

    feat: corrects btn dependencies

--- components/login-button.tsx
+++ components/login-button.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { Button, EIConTypes } from '@ost-cas-fee-adv-23-24/elbmum-design';
 import { signIn } from 'next-auth/react';
 import { Button, EIConTypes } from '@ost-cas-fee-adv-23-24/elbmum-design';
 
