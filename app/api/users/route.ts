1073fac9e37e18f656e4889798675e80ece5a684
Bladimir Ardiles <bladimir.ardilescruz@swisscom.com>
2024-02-17 16:54:30 +0100

    feat: cleans lint warnings. v4

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,8 +1,8 @@
-import { NextApiRequest } from 'next';
+import { NextRequest } from 'next/server';
 
-export const GET = async (request: NextApiRequest): Promise<Response> => {
+export const GET = async (request: NextRequest): Promise<Response> => {
   return new Response('test');
 };
-export const PATCH = async (request: NextApiRequest): Promise<Response> => {
+export const PATCH = async (request: NextRequest): Promise<Response> => {
   return new Response('test');
 };
