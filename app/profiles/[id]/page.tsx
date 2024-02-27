6bc380db934b1465910fa11b372cbd3e408a6e06
Patrick Lehmann <patrick.lehmann@intergga.ch>
2024-02-27 13:56:34 +0100

    create directory for header component

--- app/profiles/[id]/page.tsx
+++ app/profiles/[id]/page.tsx
@@ -1,4 +1,4 @@
-import Header from '@/components/Header';
+import Header from '@/components/header/Header';
 import { getMumbleUserByIdentifier } from '@/utils/helpers/users/getMumbleUserByIdentifier';
 import { notFound } from 'next/navigation';
 
