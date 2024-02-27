b8de179e4c40d7caaf2ab20eac214652d88e6812
Patrick Lehmann <patrick.lehmann@intergga.ch>
2024-02-27 13:58:41 +0100

    rename profile path from id to identifier

--- app/profiles/[identifier]/page.tsx
+++ app/profiles/[identifier]/page.tsx
@@ -2,11 +2,13 @@ import Header from '@/components/header/Header';
 import { getMumbleUserByIdentifier } from '@/utils/helpers/users/getMumbleUserByIdentifier';
 import { notFound } from 'next/navigation';
 
-export default async function Profile(context: { params: { id: number } }) {
+export default async function Profile(context: {
+  params: { identifier: number };
+}) {
   // TODO: redirect to profile/me if userID === current signed in userID
   try {
     const profileData = await getMumbleUserByIdentifier(
-      context.params.id.toString(),
+      context.params.identifier.toString(),
     );
 
     return <Header user={profileData} />;
