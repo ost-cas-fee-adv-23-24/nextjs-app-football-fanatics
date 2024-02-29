fdf1d6a2ca37562ecb72c04f4215d32f39798531
Bladimir Ardiles <bladimir.ardilescruz@swisscom.com>
2024-02-29 15:36:36 +0100

    feat: implements postText component

---
+++ components/post-text/PostText.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Link from 'next/link';
+
+interface IProps {
+  text: string;
+}
+
+const PostText = ({ text }: IProps) => {
+  const regexExp = /#[\p{L}\p{M}0-9_]+/gu;
+
+  const getAndRenderHashTags = (text: string) => {
+    const matches = text.match(regexExp);
+    if (!matches) return null;
+    return (
+      <div className="mt-6 mb-6 gap-2 flex flex-wrap">
+        {matches?.map((match, index) => {
+          const searchKeyword = match.replace('#', '').toLowerCase();
+          return (
+            <Link
+              href={`/posts/tags/${searchKeyword}`}
+              className="text-violet-600 text-xl leading-[1.40] font-bold"
+              key={`${match}-${index}`}
+            >
+              {match}
+            </Link>
+          );
+        })}
+      </div>
+    );
+  };
+
+  const replaceHashtags = (text: string) => {
+    return text.replace(regexExp, (match: string, hashtag: string): string => {
+      const searchKeyword = match.replace('#', '').toLowerCase();
+      // this is not jsx. it's a simple string
+      return `<a class="text-violet-600" href="/posts/hashtag/${searchKeyword}">${match}</a>`;
+    });
+  };
+
+  return (
+    <div className="text-slate-600 font-poppins not-italic font-medium text-lg leading-[1.40]">
+      <div dangerouslySetInnerHTML={{ __html: replaceHashtags(text) }}></div>
+      {getAndRenderHashTags(text)}
+    </div>
+  );
+};
+
+export default PostText;
