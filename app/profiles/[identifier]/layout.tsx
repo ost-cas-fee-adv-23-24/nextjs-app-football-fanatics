844ca1cfcdaee9011111f31cc0edd65fb00975b7
Patrick Lehmann <patrick.lehmann@intergga.ch>
2024-04-09 20:22:10 +0200

    extract Header and ProfileFollow to layout.tsx

---
+++ app/profiles/[identifier]/layout.tsx
@@ -0,0 +1,51 @@
+import { auth } from "@/app/api/auth/[...nextauth]/auth";
+import Header from "@/components/header/Header";
+import ProfileFollow from "@/components/profile-follow/ProfileFollow";
+import { getAllFollowers } from "@/utils/helpers/followers/getFollowers";
+import { getMumbleUserByIdentifier } from "@/utils/helpers/users/getMumbleUserByIdentifier";
+import { IMumbleFollowers } from "@/utils/interfaces/mumbleFollowers.interface";
+import { notFound } from "next/navigation";
+
+interface IProfileLayoutProps {
+  children: React.ReactNode;
+  params: { identifier: number };
+}
+
+
+export default async function ProfileLayout({ children, params }: IProfileLayoutProps) {
+  const currentProfileUserIdentifier = params.identifier.toString();
+
+  const session = await auth();
+
+  const userFollowers: IMumbleFollowers[] = await getAllFollowers({
+    identifier: currentProfileUserIdentifier,
+  });
+
+  try {
+    const profileData = await getMumbleUserByIdentifier(
+      currentProfileUserIdentifier,
+    );
+
+    return (
+      <div className="mx-auto bg-slate-100 pt-8">
+        <div className="global-width  mx-auto py-8">
+          <Header user={profileData} />
+          {session && (
+            <div className="mt-8 mb-4">
+              <ProfileFollow
+                loggedInUserIdentifier={session.user.identifier}
+                profileIdentifier={currentProfileUserIdentifier}
+                followers={userFollowers}
+                revalidationPath={`/profiles/${currentProfileUserIdentifier}`}
+              />
+            </div>
+          )}
+          {children}
+        </div>
+      </div>
+    );
+  } catch (error) {
+    return notFound();
+  }
+
+}
--- app/profiles/[identifier]/page.tsx
+++ app/profiles/[identifier]/page.tsx
@@ -1,66 +1,32 @@
-import Header from '@/components/header/Header';
-import { getMumbleUserByIdentifier } from '@/utils/helpers/users/getMumbleUserByIdentifier';
-import { notFound } from 'next/navigation';
-import ProfileFeed from '@/components/profile-feed/ProfileFeed';
-import { IParamsOnlyIdentifierCtx } from '@/utils/interfaces/general';
 import { auth } from '@/app/api/auth/[...nextauth]/auth';
-import { getAllFollowers } from '@/utils/helpers/followers/getFollowers';
-import React from 'react';
-import { IMumbleFollowers } from '@/utils/interfaces/mumbleFollowers.interface';
-import ProfileFollow from '@/components/profile-follow/ProfileFollow';
+import ProfileFeed from '@/components/profile-feed/ProfileFeed';
 import ProfileSwitch from '@/components/profile-switch/ProfileSwitch';
+import { IParamsOnlyIdentifierCtx } from '@/utils/interfaces/general';
 
 export default async function Profile(context: IParamsOnlyIdentifierCtx) {
   const currentProfileUserIdentifier = context.params.identifier.toString();
 
   const session = await auth();
 
-  const userFollowers: IMumbleFollowers[] = await getAllFollowers({
-    identifier: currentProfileUserIdentifier,
-  });
+  return (
 
-  try {
-    const profileData = await getMumbleUserByIdentifier(
-      currentProfileUserIdentifier,
-    );
+    <>
+      <div className="mt-8 mb-4">
+        <ProfileSwitch
+          redirectionDelay={500}
+          selectedTab={0}
+          userIdentifier={currentProfileUserIdentifier}
+          showSuggestions={!!(
+            session &&
+            currentProfileUserIdentifier === session.user.identifier
+          )} />
+      </div><ProfileFeed
+        creators={[currentProfileUserIdentifier]}
+        userIdentifier={currentProfileUserIdentifier}
+        subscribeToNewestPost={true}
+        fetchOnlyOneBatch={false} />
+    </>
 
-    return (
-      <div className="mx-auto bg-slate-100 pt-8">
-        <div className="global-width  mx-auto py-8">
-          <Header user={profileData} />
-          {session && (
-            <div className="mt-8 mb-4">
-              <ProfileFollow
-                loggedInUserIdentifier={session.user.identifier}
-                profileIdentifier={currentProfileUserIdentifier}
-                followers={userFollowers}
-                revalidationPath={`/profiles/${currentProfileUserIdentifier}`}
-              />
-            </div>
-          )}
-          <div className="mt-8 mb-4">
-            <ProfileSwitch
-              redirectionDelay={500}
-              selectedTab={0}
-              userIdentifier={currentProfileUserIdentifier}
-              showSuggestions={
-                !!(
-                  session &&
-                  currentProfileUserIdentifier === session.user.identifier
-                )
-              }
-            />
-          </div>
-          <ProfileFeed
-            creators={[currentProfileUserIdentifier]}
-            userIdentifier={currentProfileUserIdentifier}
-            subscribeToNewestPost={true}
-            fetchOnlyOneBatch={false}
-          />
-        </div>
-      </div>
-    );
-  } catch (error) {
-    return notFound();
-  }
+
+  );
 }
