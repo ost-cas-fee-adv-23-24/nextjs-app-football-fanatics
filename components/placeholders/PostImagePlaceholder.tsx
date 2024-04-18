c3f49aab16c6a490e6446c2a6926f8d2bba74efe
Bladimir Ardiles <bladimir.ardilescruz@swisscom.com>
2024-04-18 11:21:52 +0200

    feat: adds interface to postImagePlaceholder component

--- components/placeholders/PostImagePlaceholder.tsx
+++ components/placeholders/PostImagePlaceholder.tsx
@@ -1,18 +1,13 @@
-export const PostImagePlaceholder = ({
-  text,
-  pulse = true,
-}: {
+interface IProps {
   text?: string;
   pulse?: boolean;
-}) => {
-  let topClasses =
-    'mumble-image bg-slate-200 rounded-2xl relative h-0 overflow-hidden group border-1 border-slate-400';
-  if (pulse) {
-    topClasses += ' animate-pulse';
-  }
+}
 
+export const PostImagePlaceholder = ({ text, pulse = true }: IProps) => {
   return (
-    <div className={topClasses}>
+    <div
+      className={`mumble-image bg-slate-200 rounded-2xl relative h-0 overflow-hidden group border-1 border-slate-400 ${pulse ? 'animate-pulse' : ''}`}
+    >
       {text ? (
         <div
           className="absolute w-full p-2 text-center text-slate-600 font-poppins not-italic font-medium text-xs lg:text-md"
