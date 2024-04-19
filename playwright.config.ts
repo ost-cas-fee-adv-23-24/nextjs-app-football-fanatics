672b7a02b3396cb9db2f587de39e837b396aa3e9
Patrick Lehmann <patrick.lehmann@intergga.ch>
2024-04-19 22:40:30 +0200

    chore: Add test command to package.json and configure webServer in playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig, devices } from '@playwright/test';
+import envVariables from './config/env';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
+const PORT = envVariables.PORT;
+
+const baseURL = `http://localhost:${PORT}`;
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -15,16 +14,19 @@ export default defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  // retries: 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
+  webServer: {
+    command: 'npm run dev',
+    url: baseURL,
+  },
+
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
