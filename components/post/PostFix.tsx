034a653623902382a6c2343d13037ee05efa9649
Bladimir Ardiles <bladimir.ardilescruz@swisscom.com>
2024-04-20 10:52:29 +0200

    feat: adds gap between posts into calculation to get final scroll area height. eliminates problem with setting state while rendering - postFixLoader

--- components/post/PostFix.tsx
+++ components/post/PostFix.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { PostCard } from '@/components/post-card/PostCard';
 import PostActionsBar from '@/components/post-actions-bar/PostActionsBar';
 import { IPostItem } from '@/utils/interfaces/mumblePost.interface';
 import { PostCardFix } from '@/components/post-card/PostCardFix';
@@ -11,7 +10,7 @@ interface IProps {
 export const PostFix = ({ postData }: IProps) => {
   return (
     <div
-      className="bg-white py-8 px-12 relative rounded-2xl mb-6 w-full"
+      className="bg-white py-8 px-12 relative rounded-2xl  w-full"
       key={postData.id}
     >
       <PostCardFix
