c2a57e74a5b11c51f9b9abdc170b6f26cd810ab4
Bladimir Ardiles <bladimir.ardilescruz@swisscom.com>
2024-04-23 21:24:06 +0200

    feat: improves margins on hashtag page and full post page

--- app/posts/[identifier]/page.tsx
+++ app/posts/[identifier]/page.tsx
@@ -3,7 +3,6 @@ import { getMumblePostAction } from '@/utils/helpers/posts/getMumblePost';
 import React from 'react';
 import { PostFull } from '@/components/post-full/PostFull';
 import { auth } from '@/app/api/auth/[...nextauth]/auth';
-import { Session } from 'next-auth';
 
 export default async function Page({
   params,
@@ -14,8 +13,8 @@ export default async function Page({
   const responseService = await getMumblePostAction(identifier, true);
   const session = await auth();
   return (
-    <div className="mx-auto bg-slate-100 pt-8">
-      <div className="global-width mx-auto py-8">
+    <div className="bg-slate-100 pt-8">
+      <div className="global-width mx-auto py-8 px-8 md:px-0">
         <PostFull data={responseService} isUserAuthenticated={!!session} />
       </div>
     </div>
