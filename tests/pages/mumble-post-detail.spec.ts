a1159272496ce52eb238f3cc2c987ee69e7c004e
Patrick Lehmann <patrick.lehmann@intergga.ch>
2024-04-28 10:42:10 +0200

    add test to check detail view of a mumble post

--- tests/pages/mumble-post-detail.spec.ts
+++ tests/pages/mumble-post-detail.spec.ts
@@ -1,11 +1,12 @@
 import { expect, test } from "@playwright/test";
 
 
-test("should navigate to the detail view of an post", async ({ page }) => {
+test("should navigate to the detail view of an post and check reply buttons", async ({ page }) => {
   await page.goto("/");
   await page.getByRole('button', { name: 'Comment' }).first().click();
-  await expect(page.getByLabel('Picture Upload')).toBeVisible();
   await expect(page).toHaveURL(/\/posts\/[A-Z0-9]+/);
+  await expect(page.getByRole('button', { name: 'post-submit' })).toBeDefined();
+  await expect(page.getByText('Picture Upload')).toBeVisible();
 });
 
 
