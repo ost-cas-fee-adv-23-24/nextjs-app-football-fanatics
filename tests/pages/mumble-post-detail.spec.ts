69832c5b2065bf2b9fa9b9ac485668fdcb52fd20
Patrick Lehmann <patrick.lehmann@intergga.ch>
2024-04-28 18:04:45 +0200

    fix typo in test description

--- tests/pages/mumble-post-detail.spec.ts
+++ tests/pages/mumble-post-detail.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "@playwright/test";
 
 
-test("should navigate to the detail view of an post and check reply buttons", async ({ page }) => {
+test("should navigate to the detail view of a post and check reply buttons", async ({ page }) => {
   await page.goto("/");
   await page.getByRole('button', { name: 'Comment' }).first().click();
   await expect(page).toHaveURL(/\/posts\/[A-Z0-9]+/);
