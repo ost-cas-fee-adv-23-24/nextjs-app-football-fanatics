7d17da0be7bbebb7d1e41db9113ad9b6f4c4cae1
Patrick Lehmann <patrick.lehmann@intergga.ch>
2024-04-28 19:27:15 +0200

    Add test for cannot-like-post

---
+++ tests/pages/cannot-like-post.spec.ts
@@ -0,0 +1,11 @@
+import { expect, test } from "@playwright/test";
+
+
+test("cannot like post when user is not logged in", async ({ page }) => {
+  await page.goto("/");
+  await page.getByRole('button', { name: 'Like' }).first().click();
+
+  await expect(page.getByText('You need to be logged in to like a post')).toBeVisible();
+
+});
+
