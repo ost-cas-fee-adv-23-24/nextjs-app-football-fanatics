7f50f4a8c3c81776a57d4751ce1fdd4196ed3160
Bladimir Ardiles <bladimir.ardilescruz@swisscom.com>
2024-04-28 17:10:42 +0200

    feat: removes rounded corners from imagePostPlaceholder on mobile devices

--- components/placeholders/PostImagePlaceholder.tsx
+++ components/placeholders/PostImagePlaceholder.tsx
@@ -6,7 +6,7 @@ interface IProps {
 export const PostImagePlaceholder = ({ text, pulse = true }: IProps) => {
   return (
     <div
-      className={`mumble-image bg-slate-200 rounded-2xl relative h-0 overflow-hidden group border-1 border-slate-400 ${pulse ? 'animate-pulse' : ''}`}
+      className={`mumble-image bg-slate-200 md:rounded-2xl relative h-0 overflow-hidden group border-1 border-slate-400 ${pulse ? 'animate-pulse' : ''}`}
     >
       {text ? (
         <div className="absolute w-full p-2 text-center text-slate-600 font-poppins not-italic font-medium text-xs lg:text-md top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
