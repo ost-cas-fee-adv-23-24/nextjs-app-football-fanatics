eb951553dfd8a89a1459719fdd1d3c8dcc50f397
Patrick Lehmann <patrick.lehmann@intergga.ch>
2024-04-28 19:28:38 +0200

    Add test for login button visibility when user is not logged in

---
+++ tests/pages/login-button.spec.ts
@@ -0,0 +1,9 @@
+import { expect, test } from "@playwright/test";
+
+
+test("should display login button when user is not logged in", async ({ page }) => {
+  await page.goto("/");
+  await expect(page.getByLabel('Login')).toBeVisible();
+});
+
+
