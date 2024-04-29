ef68dda852e55d1d4ab1ea8d4da7678ae1c11848
Bladimir Ardiles <bladimir.ardilescruz@swisscom.com>
2024-04-29 20:03:14 +0200

    feat: removes test class

--- components/placeholders/ProfileHeaderPlaceholder.tsx
+++ components/placeholders/ProfileHeaderPlaceholder.tsx
@@ -6,7 +6,7 @@ export const ProfileHeaderPlaceholder = () => {
   return (
     <div>
       <div className="mb-12">
-        <div className="relative mumble-image bg-red">
+        <div className="relative mumble-image">
           <div className="absolute top-0 left-0 right-0 bottom-0">
             <PostImagePlaceholder />
             <AvatarPlaceholder />
