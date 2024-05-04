1e6907e8acb0e34b4ae66ddaaa074b172466ee34
Patrick Lehmann <patrick.lehmann@intergga.ch>
2024-05-04 11:40:56 +0200

    Add environment variables for test user credentials

--- config/env.ts
+++ config/env.ts
@@ -11,6 +11,8 @@ const envSchema = z.object({
   ZITADEL_CLIENT_ID: z.string(),
   MUMBLE_API_URL: z.string(),
   ENVIRONMENT: z.string(),
+  TEST_USER_NAME: z.string(),
+  TEST_USER_PASSWORD: z.string(),
 });
 
 const envVariables = envSchema.parse(process.env);
