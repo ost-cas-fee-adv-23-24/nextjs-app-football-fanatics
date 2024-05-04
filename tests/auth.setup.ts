ebb5580714d420f84ef9c71bf2f90ee2d5a4a4f0
Patrick Lehmann <patrick.lehmann@intergga.ch>
2024-05-04 11:40:25 +0200

    remove login credentials and use env variables instead

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -2,19 +2,21 @@ import { expect, test as setup } from '@playwright/test';
 
 const authFile = 'playwright/.auth/user.json';
 const urlRegex = /https:\/\/cas-fee-adv-ed1ide\.zitadel\.cloud\/ui\/login\/login\?authRequestID=\d+/
+const username = process.env.TEST_USER_NAME || '';
+const password = process.env.TEST_USER_PASSWORD || '';
+
 
 setup('authenticate', async ({ page }) => {
   setup.slow();
   await page.goto("/");
-  // check content is loaded
-  await page.getByRole('button', { name: 'Comment' }).first();
+  await expect(page.getByRole('button', { name: 'Comment' }).first(), "content should be loaded");
   await page.getByLabel('Login').click();
   await page.waitForURL(urlRegex);
   await page.getByPlaceholder('username@domain').fill(
-    'patrick.lehmann@ost.ch');
+    username);
   await page.getByRole('button', { name: 'Next' }).click();
   await page.getByLabel('Password').click();
-  await page.getByLabel('Password').fill('&aScKvvwq4^wv2Jwhf3xw8iY9wGD6@');
+  await page.getByLabel('Password').fill(password);
   await page.getByRole('button', { name: 'Next' }).click();
   await expect(page.getByLabel('Logout')).toBeVisible();
 
