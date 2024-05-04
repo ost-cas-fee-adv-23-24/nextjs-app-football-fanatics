9d81173e194ba275180c497f48c2e19b0ee07df8
Bladimir Ardiles <bladimir.ardilescruz@swisscom.com>
2024-05-04 19:08:59 +0200

    feat:  improves configs

--- config/index.ts
+++ config/index.ts
@@ -1,37 +1,4 @@
-import { ClientAuthenticationMethod } from 'oauth4webapi';
-
-export interface IConfig {
-  environment: string;
-  nextSecret: string;
-  nextAuthUrl: string;
-  sessionStrategy: 'jwt' | 'database' | undefined;
-  sessionMaxAge: number;
-  zitadel: {
-    clientId: string;
-    authority: string;
-    scope: string;
-    checks: ('pkce' | 'state' | 'none')[];
-    tokenEndpointAuthMethod: ClientAuthenticationMethod;
-  };
-  mumble: {
-    host: string;
-  };
-  avatar: {
-    fileNameUploader: string;
-  };
-  feed: {
-    defaultAmount: number;
-  };
-  trustedDomains: string[];
-  cacheRules: {
-    allUsersData: number;
-    userProfileData: number;
-  };
-  testing: {
-    username: string;
-    password: string;
-  };
-}
+import { IConfig } from '@/config/utils/interface/config.interface';
 
 // only use in backend!!!!
 const config = {
