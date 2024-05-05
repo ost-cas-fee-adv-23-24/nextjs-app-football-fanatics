0d3292681072e3650ed1afdc364af0bef6d43d2e
Bladimir Ardiles <bladimir.ardilescruz@swisscom.com>
2024-05-05 19:34:42 +0200

    feat: improves post api endpoints. usage of mumblePost instance. removes unused endpoint for post fetch (app uses instead a server action)

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,11 +1,8 @@
-import config from '@/config';
-import { MumblePostService } from '@/services/Mumble/MumblePost';
 import { NextRequest, NextResponse } from 'next/server';
 import { auth } from '../auth/[...nextauth]/auth';
 import { IGetPostsParams } from '@/utils/interfaces/mumblePost.interface';
 import frontendConfig from '@/config/configFrontend';
-
-const dataSource = new MumblePostService(config.mumble.host);
+import mumblePostService from '@/services/Mumble/MumblePost';
 
 export const GET = async (request: NextRequest): Promise<Response> => {
   const searchParams = request.nextUrl.searchParams;
@@ -42,32 +39,13 @@ export const GET = async (request: NextRequest): Promise<Response> => {
 
   const session = await auth();
   try {
-    const response = await dataSource.getPosts({
+    const response = await mumblePostService.getPosts({
       token: session ? session.accessToken : '',
       data: mumbleNextUrl ? { mumbleNextUrl } : params,
     });
     return NextResponse.json(response);
   } catch (error) {
     console.log('Error fetching posts');
-    throw error;
-  }
-};
-
-export const POST = async (request: NextRequest): Promise<Response> => {
-  const session = await auth();
-
-  try {
-    if (!session) throw new Error('Unauthorized');
-    const formData = await request.formData();
-    // @ts-ignore
-    const token = session ? session.accessToken : '';
-    const response = await dataSource.createPost({
-      token,
-      formData,
-    });
-    return NextResponse.json(response);
-  } catch (error) {
-    console.log('Error while creating post');
     throw error;
   }
 };
