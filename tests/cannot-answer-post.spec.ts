c9e970517c936ed04c5000ceb7840fb7bfbd2ab5
Patrick Lehmann <patrick.lehmann@intergga.ch>
2024-05-08 07:25:35 +0200

    improve test description

--- tests/cannot-answer-post.spec.ts
+++ tests/cannot-answer-post.spec.ts
@@ -1,11 +1,12 @@
 import { expect, test } from "@playwright/test";
 
 
-test("should create a reply to a post", async ({ page }) => {
+test("cannot create a reply to a post when user is not logged in", async ({ page }) => {
   await page.goto("/");
   await page.getByRole('button', { name: 'Comment' }).first().click();
 
   await expect(page).toHaveURL(/\/posts\/[A-Z0-9]+/);
+  // buttons should not be rendered when user is not logged in
   await expect(page.getByRole('button', { name: 'post-submit' })).not.toBeVisible();
   await expect(page.getByText('Picture Upload')).not.toBeVisible();
 
--- tests/cannot-write-post.spec.ts
+++ tests/cannot-write-post.spec.ts
@@ -1,10 +1,11 @@
 import { expect, test } from "@playwright/test";
 
 
-test("cannot write post because buttons are not rendered", async ({ page }) => {
+test("cannot write post when user is not logged in", async ({ page }) => {
   await page.goto("/");
   await page.getByRole('button', { name: 'Comment' }).first().isVisible();
 
+  // buttons should not be rendered when user is not logged in
   await expect(page.getByRole('button', { name: 'post-submit' })).not.toBeVisible();
   await expect(page.getByText('Picture Upload')).not.toBeVisible();
 });
