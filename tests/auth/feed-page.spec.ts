3ace331034828b0d5c3757aa32ddc723820227e5
Patrick Lehmann <patrick.lehmann@intergga.ch>
2024-05-08 07:25:01 +0200

    improve regex pattern

--- tests/auth/feed-page.spec.ts
+++ tests/auth/feed-page.spec.ts
@@ -1,9 +1,11 @@
 import { expect, test } from "@playwright/test";
 
 
-test("should redirect home page to feed page when user is logged in", async ({ page }) => {
+test("should redirect from home page to feed or feed new page when user is logged in", async ({ page }) => {
   await page.goto("/");
-  await expect(page).toHaveURL(/\/feed/);
+
+  await expect(page).toHaveURL(/\/feed(\/new)?/);
+
 });
 
 
